1bdfafdc57abfa19422609afd5bfb33b20605bfa
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-04-13 19:51:24 +0300

    Initial Commit

---
+++ AnimationSwiftUI/AnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSwiftUIApp.swift
+//  AnimationSwiftUI
+//
+//  Created by ALEKSEY SUSLOV on 13.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
